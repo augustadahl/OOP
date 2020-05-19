94acc365942446616caf17ed1a27af931f0e988d
augustadahl <august.adahl@outlook.com>
2020-05-19 13:29:08 +0200

    s

---
+++ src/oppethus/SimpleAddition.java
@@ -0,0 +1,19 @@
+package oppethus;
+
+public class SimpleAddition {
+
+	public static void main(String[] args) {
+		
+		int a = 50;
+		
+		int b = 4;
+		
+		int x = a + b;
+		
+		System.out.println(x);
+		
+		
+		
+	}
+	
+}
